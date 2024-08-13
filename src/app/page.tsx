e12c2e267aec7cfd70d5e1488c0bebb1b0b46b1d
Bernas01 <felipebalmeida0108@gmail.com>
2024-08-13 19:41:32 -0300

    commit felipe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-between">
       <nav className="absolute right-2 top-2">
         <ModeToggle />
       </nav>
 
       <aside className="hidden lg:block">
-        <Image
-          src="/login.png"
-          alt="Imagem de login"
-          width={554}
-          height={832}
-        />
+        <Image src="/login.png" alt="imagem de login" width={554} height={832} />
       </aside>
 
       <section className="flex flex-col items-center w-full gap-4">
-        <svg
-          width="75"
-          height="75"
-          viewBox="0 0 75 75"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48" />
         </svg>
-        <h1 className="text-4xl font-entrabold uppercase">Sphere</h1>
+        <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
 
         <Tabs>
           <TabsList defaultValue="entrar" className="min-w-[200px]">
@@ -76,8 +65,12 @@ export default function Home() {
               <Button type="submit">criar conta</Button>
             </form>
           </TabsContent>
+
         </Tabs>
+
+
       </section>
+
     </main>
   );
 }
